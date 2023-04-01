996a49b8cd4ec3762c31c0c23b31053322d98c15
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-04-01 09:03:31 +0300

    first sample Calc

--- src/CalcDay1_backend/main.mo
+++ src/CalcDay1_backend/main.mo
@@ -1,5 +1,68 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Float "mo:base/Float";
+
+actor CALCULATOR {
+
+  //declare and initialize a variable counter of type Float
+  var counter : Float = 0.0;
+
+  //add the number from user to the counter
+  public func add(num : Float) : async Float {
+    counter += num;
+    return counter;
   };
+
+  //subtract the imput number from the counter
+  public func sub(num : Float) : async Float {
+    counter -= num;
+    return counter;
+  };
+
+  //multiply the input number by the counter
+  public func mul(num : Float) : async Float {
+    counter *= num;
+    return counter;
+
+  };
+
+  //div the input number by the counter
+  public func div(num : Float) : async ?Float {
+    if (num == 0) {
+      null;
+    } else {
+      counter /= num;
+      return ?counter;
+    };
+  };
+
+  //square root the input number by the counter
+  public func sqrt() : async Float {
+    counter **= 1 / 2;
+    return counter;
+
+  };
+
+  //power the input number by the counter
+  public func pow(num : Float) : async Float {
+    counter **= num;
+    return counter;
+
+  };
+
+  //floor the input number by the counter
+  public func floor() : async Int {
+    counter := Float.floor(counter);
+    return Float.toInt(counter);
+
+  };
+
+  //reset the input number by the counter
+  public func reset() : async () {
+    counter := 0.0;
+  };
+
+  //see the input number by the counter
+  public query func see() : async Float{
+    return counter;
+  };
+
 };
--- src/CalcDay1_frontend/src/index.js
+++ src/CalcDay1_frontend/src/index.js
@@ -1,19 +1,129 @@
 import { CalcDay1_backend } from "../../declarations/CalcDay1_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+//when the window loads for the first time
+window.addEventListener('load', async () => {
 
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await CalcDay1_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+  const result = await CalcDay1_backend.see()
+  document.getElementById("result").innerHTML = result
+  
 });
+
+
+
+//allow user to type using the keyboard on the screen
+document.querySelector("#calc")
+.addEventListener("click", async (e) =>{
+  if(e.target.className.includes("number")){
+
+    const index = e.target.id 
+    document.getElementById("inputValue").value += index
+  }
+})
+
+
+
+//perform addition
+document.querySelector("#plus")
+.addEventListener("click", async (e) =>{
+  const num = parseFloat(document.getElementById("inputValue").value)
+              
+  //call the add method of the canister
+  const Result = await CalcDay1_backend.add(num)
+
+  document.getElementById("result").innerHTML = Result
+
+})
+
+
+//perform subtraction
+document.querySelector("#subtract")
+.addEventListener("click", async () =>{
+  
+  const num = parseFloat(document.getElementById("#inputValue").value)
+  console.log("sub num:",num)
+              
+  //call the sub method of the canister
+  //const Result = await CalcDay1_backend.sub(num)
+
+ // document.getElementById("result").innerHTML = Result
+
+})
+
+
+//perform multiply
+document.querySelector("#multiply")
+.addEventListener("click", async (e) =>{
+  const num = parseFloat(document.getElementById("inputValue").value)     
+
+  //call the mul method of the canister
+  const Result = await CalcDay1_backend.mul(num)
+
+  document.getElementById("result").innerHTML = Result
+
+})
+
+//perform division
+document.querySelector("#divide")
+.addEventListener("click", async (e) =>{
+  const num = parseFloat(document.getElementById("inputValue").value)       
+
+  //call the div method of the canister
+  const Result = await CalcDay1_backend.div(num)
+
+  document.getElementById("result").innerHTML = Result
+
+})
+
+
+//perform square root
+document.querySelector("#root")
+.addEventListener("click", async (e) =>{
+
+  //call the sqrt method of the canister
+  const Result = await CalcDay1_backend.sqrt()
+ 
+  document.getElementById("result").innerHTML = Result
+
+})
+
+
+//perform power
+document.querySelector("#power")
+.addEventListener("click", async (e) =>{
+  const num = parseFloat(document.getElementById("inputValue").value)
+              
+
+  //call the pow method of the canister
+  const Result = await CalcDay1_backend.pow(num)
+
+  document.getElementById("result").innerHTML = Result
+
+})
+
+
+//perform floor
+document.querySelector("#floor")
+.addEventListener("click", async (e) =>{
+
+  //call the floor method of the canister
+  const Result = await CalcDay1_backend.floor()
+
+  document.getElementById("result").innerHTML = Result
+
+})
+
+
+//perform reset
+document.querySelector("#reset")
+.addEventListener("click", async (e) =>{
+
+  //call the reset method of the canister
+  await CalcDay1_backend.reset()
+  const result = await CalcDay1_backend.see()
+
+  document.getElementById("result").innerHTML = result
+})
+
+
+
